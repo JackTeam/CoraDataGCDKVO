ca6f9b87e4bc5012ecbf9014657595769220d9d8
Jack <xhzengAIB@gmail.com>
2016-03-15 17:05:42 +0800

    修改描述

--- CoraDataGCDKVO/CoreData/NSManagedObject+JSON.h
+++ CoraDataGCDKVO/CoreData/NSManagedObject+JSON.h
@@ -2,7 +2,8 @@
 //  NSManagedObject+JSON.h
 //  SignificantDates
 //
-//  Created by Chris Wagner on 7/8/12.
+//  Created by 曾 宪华 on 13-8-26.
+//  Copyright (c) 2013年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
 
 #import <CoreData/CoreData.h>
